96963f7c52b63b5890fba55eac3bccc0e829eb0f
Serge Najimov <najimovserge@gmail.com>
2020-07-29 12:33:31 +0200

    my first deploy

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello To Do App</h1>
+      <h2>Author: Serge Najimov</h2>
+    </div>
+  );
+}
+
+export default App;
