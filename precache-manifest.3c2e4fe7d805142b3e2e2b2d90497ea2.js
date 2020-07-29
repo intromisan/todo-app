7eb82b16dc6633b9b23b07c7df635325f027feeb
Serge Najimov <najimovserge@gmail.com>
2020-07-29 12:35:39 +0200

    Updates

---
+++ precache-manifest.3c2e4fe7d805142b3e2e2b2d90497ea2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baa048a32c31ba1875aaf7312df73646",
+    "url": "/intromisan/todo-app/index.html"
+  },
+  {
+    "revision": "b88616c7d65121c7f1ee",
+    "url": "/intromisan/todo-app/static/css/main.33a61a91.chunk.css"
+  },
+  {
+    "revision": "3f0a780ffe2841536d7e",
+    "url": "/intromisan/todo-app/static/js/2.475ec287.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/intromisan/todo-app/static/js/2.475ec287.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b88616c7d65121c7f1ee",
+    "url": "/intromisan/todo-app/static/js/main.947afc42.chunk.js"
+  },
+  {
+    "revision": "39cf6d78340bbe315170",
+    "url": "/intromisan/todo-app/static/js/runtime-main.54b334a1.js"
+  }
+]);
